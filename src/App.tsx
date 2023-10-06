82664b2bb9a1c305bb159d5c7323e272cbf6f5ad
neIsonp <nelsonponte37@gmail.com>
2023-10-06 09:52:00 +0000

    without react query

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useFecth } from "./hooks/useFecth";
+
+type Repository = {
+  full_name: string;
+  description: string;
+};
+
+function App() {
+  const { data: repositories, isFetching } = useFecth<Repository[]>(
+    "users/neisonp/repos"
+  );
+
+  return (
+    <ul>
+      {isFetching && <p>Carregando...</p>}
+      {repositories?.map((repo) => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default App;
